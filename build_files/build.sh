439dd374239a41ef8b870b55e0977ecc16ba9279
Aman B Singh <amanb.singh21@gmail.com>
2026-02-07 22:51:34 +0000

    initial build

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,11 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+#dnf5 install -y tmux 
+
+dnf5 remove gnome-\*
+
+dnf5 group install cosmic-desktop
 
 # Use a COPR Example:
 #
@@ -21,4 +25,4 @@ dnf5 install -y tmux
 
 #### Example for enabling a System Unit File
 
-systemctl enable podman.socket
+#systemctl enable podman.socket
